7ef693a845e8e571ab9c356954d4ffea06d770ab
MichaelTayler <79035992+MichaelTayler@users.noreply.github.com>
2021-02-26 06:24:57 +0100

    Update i2s.h

--- Teensy_4.0/i2s.h
+++ Teensy_4.0/i2s.h
@@ -1,1 +1,6 @@
+// Sine output test MCVE for SuperAudioBoard and Teensy 4.0.  
+// Frequency limited to 48 kHz subharmonics.
+// Upload via Arduino IDE and Teensyduino
+// CS4272 I2C and I2S code TODO
+// Written by Michael Tayler.  Updated 26/02/2021.
 
