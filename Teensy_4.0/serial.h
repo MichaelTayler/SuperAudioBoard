db63489b4421ad05915b2aa8530cdafecc38ab4c
Michael Tayler <79035992+MichaelTayler@users.noreply.github.com>
2021-03-20 02:12:23 +0100

    Update serial.h

--- Teensy_4.0/serial.h
+++ Teensy_4.0/serial.h
@@ -2,13 +2,14 @@
 // Frequency limited to 48 kHz subharmonics.
 // Upload via Arduino IDE and Teensyduino
 // CS4272 I2C and I2S code based on https://github.com/whollender/SuperAudioBoard
-// Written by Michael Tayler.  Updated 26/02/2021.
+// Written by Michael Tayler.  Updated 19/03/2021.
 
 // To start the SuperAudioBoard output/acquisition loop, send character "A" over serial port.
 
 byte serialData[4];
 unsigned int dataLength;
 volatile unsigned int datapts;
+volatile byte sinecounter;
 
 void Serial_init(){Serial.begin(115200); delay(100);}
 void print_error(byte err){while (Serial.available() > 0){ Serial.read(); } Serial.println(err);}
@@ -26,10 +27,7 @@ void readSerialData(){
   while (Serial.available() > 0){ Serial.read(); }
  
   if(serialData[0] == 0x41){
-  state=State_ISR;
-      counter=0;
-      datapts=0;
-      dataLength = 256;
-      i2s_start();    // start sampling
+    state=State_Acquire;
+    dataLength = 10000;
   }
 }
